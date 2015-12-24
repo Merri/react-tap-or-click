f7942ffcb75fd2167389d757f5c2bea65abb93ea
Vesa Piittinen <vesa.piittinen@gmail.com>
2015-12-24 17:35:49 +0200

    Major refactor; add click buster and more abstraction

--- index.js
+++ index.js
@@ -1,5 +1,120 @@
 'use strict'
 
+// http://ariatemplates.com/blog/2014/05/ghost-clicks-in-mobile-browsers/
+var bustableClicks = []
+
+function addBuster(touchEvent) {
+    if (!touchEvent || !('changedTouches' in touchEvent)) {
+        throw new Error('No event or no changedTouches in event object')
+    }
+    bustableClicks.push({
+        element: touchEvent.target,
+        x: touchEvent.changedTouches[0].screenX,
+        y: touchEvent.changedTouches[0].screenY
+    })
+}
+
+function buster(event) {
+    if (bustableClicks.length === 0) {
+        return
+    }
+
+    var x = ~~event.screenX
+    var y = ~~event.screenY
+    var i
+    var b
+
+    var xMin = x - 50
+    var xMax = x + 50
+    var yMin = y - 50
+    var yMax = y + 50
+
+    for (i = 0; i < bustableClicks.length; i++) {
+        b = bustableClicks[i]
+        if (b.x > xMin && b.x < xMax && b.y > yMin && b.y < yMax) {
+            event.preventDefault()
+            event.stopImmediatePropagation()
+            break
+        }
+    }
+}
+
+function removeBusterByElement(element) {
+    for (var i = bustableClicks.length - 1; i >= 0; i--) {
+        if (bustableClicks[i].element === element) {
+            bustableClicks.splice(i, 1)
+        }
+    }
+}
+
+if (typeof document !== 'undefined' && 'addEventListener' in document) {
+    document.addEventListener('click', buster, true)
+}
+
+// handler cache to
+// 1) avoid unnecessary duplicates for same callback, and
+// 2) to warn in case of possible issues in renders
+var cache = []
+
+function endTouch(state) {
+    state.touches = 0
+    state.touchTimeout = null
+    removeBusterByElement(this)
+}
+
+function createHandlers(callback) {
+    if (cache.length > 10000) {
+        if (typeof console !== 'undefined' && 'log' in console) {
+            console.log('tapOrClick cache flushed after 10000 items; check your renders if this happens often')
+        }
+        cache.length = 0
+    }
+
+    var state = { touches: 0 }
+
+    var handler = {
+        callback: callback,
+        touchStart: function(event) {
+            if (event.defaultPrevented) {
+                return
+            }
+            clearTimeout(state.touchTimeout)
+            state.touches++
+            state.touchTimeout = setTimeout(endTouch.bind(event.target, state), 250)
+        },
+        touchEnd: function(event) {
+            if (event.defaultPrevented || state.touches !== 1) {
+                return
+            }
+            clearTimeout(state.touchTimeout)
+            addBuster(event)
+            event.preventDefault()
+            callback(event)
+            state.touchTimeout = setTimeout(endTouch.bind(event.target, state), 425)
+        },
+        click: function(event) {
+            if (event.defaultPrevented || state.touches > 0) {
+                return
+            }
+            callback(event)
+        }
+    }
+
+    cache.push(handler)
+
+    return handler
+}
+
+function getHandlers(callback) {
+    for (var i = 0; i < cache.length; i++) {
+        if (cache[i].callback === callback) {
+            return cache[i]
+        }
+    }
+
+    return createHandlers(callback)
+}
+
 var isFunction = (function() {
     function typeOfFn(fn) {
         return typeof fn === 'function'
@@ -9,69 +124,13 @@ var isFunction = (function() {
         return Object.prototype.toString.call(fn) === '[object Function]'
     }
 
-    // typeof is fastest way to check if a function but older IEs don't support it for that and Chrome had a bug
+    // typeof is fastest way to check if a function but older IEs don't support it for functions and Chrome had a bug
     if (typeof typeOfFn === 'function' && typeof /./ !== 'function') {
         return typeOfFn
     }
 
     return objectFn
 })()
-
-var cache = []
-
-function getCallbackHandlers(callback) {
-    // flush cache if we have too many items
-    if (cache.length > 10000) {
-        if (typeof console !== 'undefined' && 'log' in console) {
-            console.log('tapOrClick cache flushed after 10000 items; check your renders if this happens often')
-        }
-        cache.length = 0
-    }
-
-    var handler = cache.filter(function(handler) {
-        return handler.callback === callback
-    })[0]
-
-    if (!handler) {
-        var state = {}
-
-        function resetState() {
-            state.touchClick = false
-            state.touchTimeout = null
-        }
-
-        handler = {
-            callback: callback,
-            touchStart: function(event) {
-                if (event.defaultPrevented) {
-                    return
-                }
-                clearTimeout(state.touchTimeout)
-                state.touchClick = true
-                state.touchTimeout = setTimeout(resetState, 200)
-            },
-            touchEnd: function(event) {
-                if (event.defaultPrevented || !state.touchClick) {
-                    return
-                }
-                clearTimeout(state.touchTimeout)
-                event.preventDefault()
-                callback(event)
-                state.touchTimeout = setTimeout(resetState, 300)
-            },
-            click: function(event) {
-                if (event.defaultPrevented || state.touchClick) {
-                    return
-                }
-                callback(event)
-            }
-        }
-
-        cache.push(handler)
-    }
-
-    return handler
-}
 
 module.exports = function tapOrClick(callback, props) {
     if (!isFunction(callback)) {
@@ -84,7 +143,7 @@ module.exports = function tapOrClick(callback, props) {
         throw new Error('Optional second argument to tapOrClick must be a mutable object')
     }
 
-    var handlers = getCallbackHandlers(callback)
+    var handlers = getHandlers(callback)
 
     props.onTouchStart = handlers.touchStart
     props.onTouchEnd = handlers.touchEnd
